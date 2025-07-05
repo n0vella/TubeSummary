a4cf8e9836caa0c6e330b1d2165885a89b917c9c
n0vella <n0vella@outlook.com>
2025-07-05 22:50:55 +0200

    some error handling

--- src/modules/chat.ts
+++ src/modules/chat.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Message } from '..'
 import { getTranscript } from './youtube'
 import { Settings } from '..'
+import { error } from '../utils'
 
 export function useChat(): [Message[], typeof ask, boolean] {
   const [chat, setChat] = useState<Message[]>([])
@@ -37,21 +38,30 @@ export function useChat(): [Message[], typeof ask, boolean] {
       onloadend: () => setIsresponging(false),
       onabort: () => setIsresponging(false),
       onprogress: function (response) {
-        const responses = response.responseText?.split('\n')
-
-        if (!responses) return
-
+        let responses
         let result = ''
-        for (const response of responses) {
-          if (response) {
-            const parseableResponse = '{' + response.replace('data: ', '"data": ') + '}'
-            const data = JSON.parse(parseableResponse).data
-            const delta = data.choices[0].delta.content
 
-            if (delta) {
-              result += delta
+        try {
+          responses = response.responseText?.split('\n')
+
+          if (!responses) return
+
+          for (const response of responses) {
+            if (response) {
+              const parseableResponse = '{' + response.replace('data: ', '"data": ') + '}'
+              const data = JSON.parse(parseableResponse).data
+              const delta = data.choices[0].delta.content
+
+              if (delta) {
+                result += delta
+              }
             }
           }
+
+          if (!result) throw 'No result'
+        } catch {
+          error("There was a problem with model's response.", response)
+          result = "There was a problem with model's response. See console for more info"
         }
 
         setChat([
@@ -70,7 +80,14 @@ export function useChat(): [Message[], typeof ask, boolean] {
     const settings = GM_getValue<Settings>('settings')
 
     if (!settings.prompt.includes('{transcription}')) {
-      throw 'Prompt must contain "{transcription}" flag'
+      error('Prompt must contain "{transcription}" flag')
+      setChat([
+        {
+          role: 'assistant',
+          content: 'Prompt must contain "{transcription}" flag',
+        },
+      ])
+      return
     }
 
     const prompt = settings.prompt.replace('{transcription}', transcript)
