3ad5ee0160086f9cd44e7132f22ed00242f51954
n0vella <n0vella@outlook.com>
2026-08-21 16:12:29 +0200

    deps update

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import App from './App'
 import { awaitElement } from './utils'
 import { render } from 'preact'
 import './index.css'
-import { createRoot } from 'preact/compat/client'
 
 async function main() {
   const dotButton = (await awaitElement('#button-shape')) as HTMLDivElement
